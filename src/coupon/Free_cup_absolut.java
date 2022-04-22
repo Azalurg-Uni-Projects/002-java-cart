0b20174bc72a6b0b7f8194558d1245f84ff9d40e
Azalurg <patryk31415@gmail.com>
2022-04-22 20:17:40 +0200

    free cup abolute - class

--- src/coupon/Free_cup_absolut.java
+++ src/coupon/Free_cup_absolut.java
@@ -3,10 +3,26 @@ package src.coupon;
 import src.cart.Cart;
 import src.product.Product;
 
+import java.util.Objects;
+
 public class Free_cup_absolut implements Coupon {
     @Override
     public void apply(Cart cart) {
+        Product[] products = cart.getProducts();
+        boolean should_add = true;
+
         if (cart.getProductsValue() > 200){
+            for (Product product : products){
+                if (Objects.equals(product.getName(), "Red cup")) {
+                    should_add = false;
+                    break;
+                }
+            }
+        }else{
+            should_add = false;
+        }
+
+        if(should_add){
             cart.addProduct(new Product("666", "Red cup", 0));
         }
     }
