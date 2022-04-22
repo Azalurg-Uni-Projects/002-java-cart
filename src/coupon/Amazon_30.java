6f9976a418fa82fb9ccadb4d4ca4144a93b74ef0
Azalurg <patryk31415@gmail.com>
2022-04-22 20:55:25 +0200

    Amazon coupon added

--- src/coupon/Amazon_30.java
+++ src/coupon/Amazon_30.java
@@ -1,2 +1,18 @@
-package src.coupon;public class Amazon_30 {
+package src.coupon;
+
+import src.cart.Cart;
+import src.product.Product;
+
+import java.util.Objects;
+
+public class Amazon_30 implements Coupon{
+    @Override
+    public void apply(Cart cart) {
+        Product[] products = cart.getProducts();
+        for (Product product : products){
+            if (Objects.equals(product.getName(), "Amazon")) {
+                product.setDiscountPrice(product.getPrice() * 0.70);
+            }
+        }
+    }
 }
