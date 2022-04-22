ba1ac958f856a3a114f716a8b1ba547aec727208
Azalurg <patryk31415@gmail.com>
2022-04-22 14:49:27 +0200

    get coupons test

--- src/coupon/Coupon.java
+++ src/coupon/Coupon.java
@@ -1,4 +1,15 @@
 package src.coupon;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Coupon {
+    private String code;
+
+    public Coupon(){}
+    public Coupon(String code){
+        this.code = code;
+    }
 }
--- src/test/Cart_Test.java
+++ src/test/Cart_Test.java
@@ -3,6 +3,7 @@ package src.test;
 import org.junit.Before;
 import org.junit.Test;
 import src.cart.Cart;
+import src.coupon.Coupon;
 import src.product.Product;
 
 import static org.junit.Assert.*;
@@ -19,7 +20,16 @@ public class Cart_Test {
         Product[] result = cart.getProducts();
         int expected = 1;
 
-
         assertEquals("get products", result.length, expected);
+    }
+
+    @Test
+    public void get_coupon_test(){
+        String expected = "Promocja na masło";
+        cart.addCoupon(new Coupon(expected));
+        Coupon[] coupons = cart.getCoupons();
+        String result = coupons[0].getCode();
+
+        assertEquals("get coupon", result, expected);
     }
 }
