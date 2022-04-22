30e9228f992eb8ce549426cecd8da9bcaefd0cc5
Azalurg <patryk31415@gmail.com>
2022-04-22 20:44:58 +0200

    third for free - class

--- src/coupon/Third_free.java
+++ src/coupon/Third_free.java
@@ -1,2 +1,24 @@
-package src.coupon;public class Third_free {
+package src.coupon;
+
+import src.cart.Cart;
+import src.product.Product;
+
+public class Third_free implements Coupon{
+
+    @Override
+    public void apply(Cart cart) {
+        Product[] products = cart.getProducts();
+        if(products.length >= 3){
+            double min_val = products[0].getPrice();
+            int min_index = 0;
+
+            for (int i = 0; i< products.length; i++){
+                if(products[i].getPrice() < min_val){
+                    min_index = i;
+                    min_val = products[min_index].getPrice();
+                }
+            }
+            products[min_index].setDiscountPrice(0);
+        }
+    }
 }
